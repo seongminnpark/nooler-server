32fa5f31bd39c6c41da9e15682b78a851c4df55c
Seongmin Park <seongminnpark@gmail.com>
2018-07-31 10:03:25 -0400

    Decouple app with main

--- cmd/nooler/main.go
+++ cmd/nooler/main.go
@@ -1,15 +1,8 @@
 package main
 
-import (
-	"fmt"
-	"net/http"
-	"strings"
-	"database/sql"
-	"github.com/gorilla/mux"
-	_ "github.com/go-sql-driver/mysql"
-)
-
 func main() {
 	a := App()
-	a.initialize("", "", "")
+	a.Initialize("", "", "")
+
+	a.Run(":2441")
 }
--- internal/app/nooler/app.go
+++ internal/app/nooler/app.go
@@ -1,9 +1,6 @@
-package main
+package internal
 
 import (
-	"fmt"
-	"net/http"
-	"strings"
 	"database/sql"
 	"github.com/gorilla/mux"
 	_ "github.com/go-sql-driver/mysql"
