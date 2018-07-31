2b66c6c3cd8f8d921edb1090a7aab8e8c0b3a276
Seongmin Park <seongminnpark@gmail.com>
2018-07-31 16:21:58 -0400

    Finish writing tests

--- test/main_test.go
+++ test/main_test.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
 	"net/http/httptest"
 	"os"
+	"strconv"
 	"testing"
 
 	"github.com/joho/godotenv"
@@ -77,4 +81,123 @@ func checkResponseCode(t *testing.T, expected, actual int) {
 	if expected != actual {
 		t.Errorf("Expected response code %d. Got %d\n", expected, actual)
 	}
+}
+func TestGetNonExistentUser(t *testing.T) {
+	clearTable()
+
+	req, _ := http.NewRequest("GET", "/user/42", nil)
+	response := executeRequest(req)
+
+	checkResponseCode(t, http.StatusNotFound, response.Code)
+
+	var m map[string]string
+	json.Unmarshal(response.Body.Bytes(), &m)
+
+	if m["error"] != "User not found" {
+		t.Errorf("Expected the 'error' key of the response to be set to 'User not found'. Got '%s'", m["error"])
+	}
+}
+
+func TestCreateUser(t *testing.T) {
+	clearTable()
+
+	payload := []byte(`{"email":"test@test.com"}`)
+
+	req, _ := http.NewRequest("POST", "/user", bytes.NewBuffer(payload))
+	response := executeRequest(req)
+
+	checkResponseCode(t, http.StatusCreated, response.Code)
+
+	var m map[string]interface{}
+	json.Unmarshal(response.Body.Bytes(), &m)
+
+	if m["email"] != "test@test.com" {
+		t.Errorf("Expected user email to be 'test@test.com'. Got '%v'", m["email"])
+	}
+	if m["token"] != nil {
+		t.Errorf("Expected user token to be non empty.")
+	}
+	// the id is compared to 1.0 because JSON unmarshaling converts numbers to
+	// floats, when the target is a map[string]interface{}
+	if m["id"] != 1.0 {
+		t.Errorf("Expected user ID to be '1'. Got '%v'", m["id"])
+	}
+}
+
+func TestGetUser(t *testing.T) {
+	clearTable()
+
+	addUsers(1)
+
+	req, _ := http.NewRequest("GET", "/user/1", nil)
+	response := executeRequest(req)
+
+	checkResponseCode(t, http.StatusOK, response.Code)
+}
+
+func addUsers(count int) {
+	if count < 1 {
+		count = 1
+	}
+
+	for i := 0; i < count; i++ {
+		stringI := strconv.Itoa(i)
+		email := "user" + stringI + "test.com"
+		uuid := "Uuid" + stringI
+		token := "test_token"
+		statement := fmt.Sprintf("INSERT INTO users(email, uuid, token) VALUES('%s', %s, %s)", email, uuid, token)
+		app.DB.Exec(statement)
+	}
+}
+
+func TestUpdateUser(t *testing.T) {
+	clearTable()
+
+	addUsers(1)
+
+	req, _ := http.NewRequest("GET", "/user/1", nil)
+	response := executeRequest(req)
+
+	var originalUser map[string]interface{}
+	json.Unmarshal(response.Body.Bytes(), &originalUser)
+
+	payload := []byte(`{"email":"updated@test.com"}`)
+	req, _ = http.NewRequest("PUT", "/user/1", bytes.NewBuffer(payload))
+	response = executeRequest(req)
+
+	checkResponseCode(t, http.StatusOK, response.Code)
+
+	var m map[string]interface{}
+	json.Unmarshal(response.Body.Bytes(), &m)
+
+	if m["id"] != originalUser["id"] {
+		t.Errorf("Expected the id to remain the same (%v). Got %v", originalUser["id"], m["id"])
+	}
+	if m["uuid"] != originalUser["uuid"] {
+		t.Errorf("Expected the uuid to remain the same (%v). Got %v", originalUser["uuid"], m["uuid"])
+	}
+	if m["token"] != originalUser["token"] {
+		t.Errorf("Expected the token to remain the same (%v). Got %v", originalUser["token"], m["token"])
+	}
+	if m["email"] == originalUser["name"] {
+		t.Errorf("Expected the name to change from '%v' to '%v'. Got '%v'", originalUser["email"], m["name"], m["name"])
+	}
+}
+
+func TestDeleteUser(t *testing.T) {
+	clearTable()
+
+	addUsers(1)
+
+	req, _ := http.NewRequest("GET", "/user/1", nil)
+	response := executeRequest(req)
+	checkResponseCode(t, http.StatusOK, response.Code)
+
+	req, _ = http.NewRequest("DELETE", "/user/1", nil)
+	response = executeRequest(req)
+	checkResponseCode(t, http.StatusOK, response.Code)
+
+	req, _ = http.NewRequest("GET", "/user/1", nil)
+	response = executeRequest(req)
+	checkResponseCode(t, http.StatusNotFound, response.Code)
 }
