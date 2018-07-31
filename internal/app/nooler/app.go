109f250fff41a54a5b4d788cd34a13efbea9bb87
Seongmin Park <seongminnpark@gmail.com>
2018-07-31 16:43:28 -0400

    Finish user logic

--- internal/app/nooler/app.go
+++ internal/app/nooler/app.go
@@ -2,11 +2,15 @@ package nooler
 
 import (
 	"database/sql"
+	"encoding/json"
 	"fmt"
 	"log"
+	"net/http"
+	"strconv"
 
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/gorilla/mux"
+	"github.com/seongminnpark/nooler-server/internal/pkg/model"
 )
 
 type App struct {
@@ -21,6 +25,125 @@ func (app *App) Initialize(user, password, dbName string) {
 	if err != nil {
 		log.Fatal(err)
 	}
+
 	app.Router = mux.NewRouter()
+	app.initializeRoutes()
 }
-func (app *App) Run(addr string) {}
+
+func (app *App) Run(addr string) {
+	log.Fatal(http.ListenAndServe(addr, app.Router))
+}
+
+func (app *App) initializeRoutes() {
+	app.Router.HandleFunc("/users", app.getUsers).Methods("GET")
+	app.Router.HandleFunc("/user", app.createUser).Methods("POST")
+	app.Router.HandleFunc("/user/{id:[0-9]+}", app.getUser).Methods("GET")
+	app.Router.HandleFunc("/user/{id:[0-9]+}", app.updateUser).Methods("PUT")
+	app.Router.HandleFunc("/user/{id:[0-9]+}", app.deleteUser).Methods("DELETE")
+}
+
+func (app *App) getUser(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id, err := strconv.Atoi(vars["id"])
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Invalid user ID")
+		return
+	}
+	user := model.User{ID: id}
+	if err := user.GetUser(app.DB); err != nil {
+		switch err {
+		case sql.ErrNoRows:
+			respondWithError(w, http.StatusNotFound, "User not found")
+		default:
+			respondWithError(w, http.StatusInternalServerError, err.Error())
+		}
+		return
+	}
+	respondWithJSON(w, http.StatusOK, user)
+}
+
+func (app *App) getUsers(w http.ResponseWriter, r *http.Request) {
+	count, _ := strconv.Atoi(r.FormValue("count"))
+	start, _ := strconv.Atoi(r.FormValue("start"))
+
+	if count > 10 || count < 1 {
+		count = 10
+	}
+
+	if start < 0 {
+		start = 0
+	}
+
+	users, err := model.GetUsers(app.DB, start, count)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+	respondWithJSON(w, http.StatusOK, users)
+}
+
+func (app *App) createUser(w http.ResponseWriter, r *http.Request) {
+	var user model.User
+	decoder := json.NewDecoder(r.Body)
+	if err := decoder.Decode(&user); err != nil {
+		respondWithError(w, http.StatusBadRequest, "Invalid request payload")
+		return
+	}
+	defer r.Body.Close()
+	if err := user.CreateUser(app.DB); err != nil {
+		respondWithError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+	respondWithJSON(w, http.StatusCreated, user)
+}
+
+func (app *App) updateUser(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id, err := strconv.Atoi(vars["id"])
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Invalid user ID")
+		return
+	}
+
+	var user model.User
+
+	decoder := json.NewDecoder(r.Body)
+	if err := decoder.Decode(&user); err != nil {
+		respondWithError(w, http.StatusBadRequest, "Invalid resquest payload")
+		return
+	}
+	defer r.Body.Close()
+
+	user.ID = id
+
+	if err := user.UpdateUser(app.DB); err != nil {
+		respondWithError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+	respondWithJSON(w, http.StatusOK, user)
+}
+
+func (app *App) deleteUser(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id, err := strconv.Atoi(vars["id"])
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Invalid User ID")
+		return
+	}
+	user := model.User{ID: id}
+	if err := user.DeleteUser(app.DB); err != nil {
+		respondWithError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+	respondWithJSON(w, http.StatusOK, map[string]string{"result": "success"})
+}
+
+func respondWithError(w http.ResponseWriter, code int, message string) {
+	respondWithJSON(w, code, map[string]string{"error": message})
+}
+func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
+	response, _ := json.Marshal(payload)
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(code)
+	w.Write(response)
+}
--- internal/pkg/model/model.go
+++ internal/pkg/model/model.go
@@ -2,28 +2,61 @@ package model
 
 import (
 	"database/sql"
-	"errors"
+	"fmt"
 )
 
-type user struct {
+type User struct {
 	ID    int    `json:"id"`
 	UUID  string `json:"uuid"`
 	Email string `json:"email"`
 	Token string `json:"token"`
 }
 
-func (u *user) getUser(db *sql.DB) error {
-	return errors.New("getUser not implemented")
+func (user *User) GetUser(db *sql.DB) error {
+	statement := fmt.Sprintf("SELECT email, token FROM users WHERE id=%d", user.ID)
+	return db.QueryRow(statement).Scan(&user.Email, &user.Token)
 }
-func (u *user) updateUser(db *sql.DB) error {
-	return errors.New("updateUser not implemented")
+
+func (user *User) UpdateUser(db *sql.DB) error {
+	statement := fmt.Sprintf("UPDATE users SET email='%s' WHERE id=%d", user.Email, user.ID)
+	_, err := db.Exec(statement)
+	return err
 }
-func (u *user) deleteUser(db *sql.DB) error {
-	return errors.New("deleteUser not implemented")
+
+func (user *User) DeleteUser(db *sql.DB) error {
+	statement := fmt.Sprintf("DELETE FROM users WHERE id=%d", user.ID)
+	_, err := db.Exec(statement)
+	return err
 }
-func (u *user) createUser(db *sql.DB) error {
-	return errors.New("createUser not implemented")
+
+func (user *User) CreateUser(db *sql.DB) error {
+	statement := fmt.Sprintf("INSERT INTO users(email, uuid, token) VALUES('%s', '%s', '%s')",
+		user.Email, user.UUID, user.Token)
+	_, err := db.Exec(statement)
+	if err != nil {
+		return err
+	}
+	err = db.QueryRow("SELECT LAST_INSERT_ID()").Scan(&user.ID)
+	if err != nil {
+		return err
+	}
+	return nil
 }
-func getUsers(db *sql.DB, start, count int) ([]user, error) {
-	return nil, errors.New("getUsers not implemented")
+
+func GetUsers(db *sql.DB, start, count int) ([]User, error) {
+	statement := fmt.Sprintf("SELECT id, email, token FROM users LIMIT %d OFFSET %d", count, start)
+	rows, err := db.Query(statement)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+	users := []User{}
+	for rows.Next() {
+		var user User
+		if err := rows.Scan(&user.ID, &user.Email, &user.Token); err != nil {
+			return nil, err
+		}
+		users = append(users, user)
+	}
+	return users, nil
 }
